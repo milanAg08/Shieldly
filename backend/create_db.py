9c8a23dc094f60dafcbe3833f71d90b5b477b2b1
milanAg08 <milanagarwal2005@gmail.com>
2025-04-09 02:02:28 +0530

    Create a database initialization script

--- backend/create_db.py
+++ backend/create_db.py
@@ -1,9 +1,11 @@
-from .app import create_app
-from .extensions import db
+# backend/create_db.py
+
+from app import create_app
+from extensions import db
 
 # Create an application context
 app = create_app()
 with app.app_context():
-    # Create all tables
+    # Create all tables defined in models
     db.create_all()
-    print("All tables have been created successfully.")
+    print("All database tables have been created successfully.")
