d5be37d107b76ce4f9139317226b07b4ccac39e6
milanAg08 <milanagarwal2005@gmail.com>
2025-04-09 01:52:53 +0530

    create user model

---
+++ backend/ai_backend/models/user.py
@@ -0,0 +1,22 @@
+# backend/ai_backend/models/user.py
+
+from extensions import db
+from datetime import datetime
+
+class User(db.Model):
+    """User model to store basic user information"""
+    __tablename__ = 'users'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    age = db.Column(db.Integer, nullable=True)
+    language_preference = db.Column(db.String(10), default='en')
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    last_active = db.Column(db.DateTime, default=datetime.utcnow)
+    
+    # Relationships
+    interactions = db.relationship('Interaction', backref='user', lazy=True)
+    progress_records = db.relationship('Progress', backref='user', lazy=True)
+    
+    def __repr__(self):
+        return f'<User {self.username}>'
