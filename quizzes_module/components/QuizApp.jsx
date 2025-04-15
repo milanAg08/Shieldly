7642e96f7d5f2a617242af372a278016adf8d326
BIDIPTA BHATTACHARYA <bids8811@gmail.com>
2025-04-16 00:36:51 +0530

    Update QuizApp.jsx

--- quizzes_module/components/QuizApp.jsx
+++ quizzes_module/components/QuizApp.jsx
@@ -1,16 +1,30 @@
 import React, { useState } from "react";
 import MultipleChoiceQuiz from "./MultipleChoiceQuiz";
 import QuizResultsTest from "./QuizResultsTest";
+import LanguageToggle from "./LanguageToggle";
 
 export default function QuizApp() {
   const [results, setResults] = useState(null);
+  const [language, setLanguage] = useState("en");
+
+  const handleReset = () => {
+    setResults(null);
+  };
 
   return (
     <div>
+      <LanguageToggle language={language} onChange={setLanguage} />
+      
       {!results ? (
-        <MultipleChoiceQuiz onSubmit={(data) => setResults(data)} />
+        <MultipleChoiceQuiz 
+          language={language} 
+          onSubmit={(data) => setResults(data)} 
+        />
       ) : (
-        <QuizResultsTest results={results} />
+        <QuizResultsTest 
+          results={results} 
+          onReset={handleReset}
+        />
       )}
     </div>
   );
